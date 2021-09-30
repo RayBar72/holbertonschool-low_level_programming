9413ac182905299f9f932d6ee354edf470207344
RayBar72 <3963@holbertonschool.com>
2021-09-30 15:37:30 -0700

    Q6, A0

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ *Return: Alway 0 (Success)
+ */
+int main(void)
+{
+	char caracter;
+	int entero;
+	long largo;
+	long long largo2;
+	float flotante;
+
+	printf("Size of a char: %zu byte(s)\n", sizeof(caracter));
+	printf("Size of a int: %zu byte(s)\n", sizeof(entero));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(largo));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(largo2));
+	printf("Size of a float: %zu byte(s)\n", sizeof(flotante));
+	return (0);
+}
