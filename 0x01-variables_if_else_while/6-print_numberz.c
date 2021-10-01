18c6099feaa5521308d57a6cd261abc833007597
RayBar72 <3963@holbertonschool.com>
2021-10-01 15:12:05 -0700

    Q6, A0

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ *Return: Alway 0 (Success)
+ */
+int main(void)
+{
+	char la = '0';
+
+	for (la = '0'; la <= '9'; la++)
+		putchar(la);
+	putchar('\n');
+	return (0);
+}
