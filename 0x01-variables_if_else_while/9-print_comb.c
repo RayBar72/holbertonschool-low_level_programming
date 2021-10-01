ba927d62d366a3686ef5b24f0a3687cf8b29ce0d
RayBar72 <3963@holbertonschool.com>
2021-10-01 15:34:29 -0700

    Q9, A0

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ *Return: Alway 0 (Success)
+ */
+int main(void)
+{
+	int la = '0';
+
+	for (la = '0'; la <= '9'; la++)
+	{
+		putchar(la);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
