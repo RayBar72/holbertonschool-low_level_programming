a2414723add1bf64d3bbefc8f37154cc1d6606f4
RayBar72 <3963@holbertonschool.com>
2021-10-04 13:23:59 -0700

    Q101, A3

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,65 +8,16 @@
 int main(void)
 {
 
-	putchar ('a');
-	putchar ('n');
-	putchar ('d');
-	putchar (' ');
-	putchar ('t');
-	putchar ('h');
-	putchar ('a');
-	putchar ('t');
-	putchar (' ');
-	putchar ('p');
-	putchar ('i');
-	putchar ('e');
-	putchar ('c');
-	putchar ('e');
-	putchar (' ');
-	putchar ('o');
-	putchar ('f');
-	putchar (' ');
-	putchar ('a');
-	putchar ('r');
-	putchar ('t');
-	putchar (' ');
-	putchar ('i');
-	putchar ('s');
-	putchar (' ');
-	putchar ('u');
-	putchar ('s');
-	putchar ('e');
-	putchar ('f');
-	putchar ('u');
-	putchar ('l');
-	putchar ('"');
-	putchar (' ');
-	putchar ('-');
-	putchar (' ');
-	putchar ('D');
-	putchar ('o');
-	putchar ('r');
-	putchar ('a');
-	putchar (' ');
-	putchar ('K');
-	putchar ('o');
-	putchar ('r');
-	putchar ('p');
-	putchar ('a');
-	putchar ('r');
-	putchar (',');
-	putchar (' ');
-	putchar ('2');
-	putchar ('0');
-	putchar ('1');
-	putchar ('5');
-	putchar ('-');
-	putchar ('1');
-	putchar ('0');
-	putchar ('-');
-	putchar ('1');
-	putchar ('9');
-	putchar ('\n');
+	int x[] = {'a', 'n', 'd', ' ', 't', 'h', 'a', 't', ' ', 'p', 'i'
+			  , 'c', 'e', ' ', 'o', 'f', ' ', 'a', 'r', 't', ' '
+			  , 'i', 's', ' ', 'u', 's', 'e', 'f', 'u', 'l', '\"'
+			  , '-', ' ', 'D', 'o', 'r', 'a', ' ', 'K', 'o', 'r'
+			  , 'p', 'a', 'r', ',', ' ', '2', '0', '1', '5', '-'
+			  , '1', '0', '-', '1', '9'};
+	int i;
 
+	for (i = 0; i <= 58; i++)
+		putchar (x[i]);
+	putchar ('\n');
 	return (1);
 }
