497a9a1821e2c033a0bcc6274fc7baafc76e92ba
RayBar72 <3963@holbertonschool.com>
2021-10-05 09:51:48 -0700

    Q3, A1

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -7,13 +7,13 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int _islower(int c)
+int _islower(int x)
 {
 	char x;
 
 	if (x >= 'a' && x <= 'z')
 		_putchar('1');
 	else
-		-putchar('0');
+		_putchar('0');
 	return (0);
 }
