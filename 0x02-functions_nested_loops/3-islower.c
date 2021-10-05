f2c0904d206bba7b16c4aba9679873f166a8da2c
RayBar72 <3963@holbertonschool.com>
2021-10-05 10:19:26 -0700

    Q0, A4

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * int _islower(int c) - prints 1 when lowercase, else 0
+ * int _islower(int x) - prints 1 when lowercase, else 0
  *
  *
  * Return: On success 1.
@@ -10,9 +10,8 @@
 int _islower(int x)
 {
 
-	if (x >= 'a' && x <= 'z')
-		_putchar('1');
+	if (x >= 97 && x <= 122)
+		return (1);
 	else
-		_putchar('0');
-	return (0);
+		return (0);
 }
