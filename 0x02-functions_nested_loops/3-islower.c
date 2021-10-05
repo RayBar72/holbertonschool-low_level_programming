69606026a5ae46d4c3b81f110b00e84d66867a38
RayBar72 <3963@holbertonschool.com>
2021-10-05 10:25:04 -0700

    Q0, A5

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * int _islower(int x) - prints 1 when lowercase, else 0
- *
+ * _islower - prints 1 when lowercase, else 0
+ * @x: integerr to be evaluated
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
