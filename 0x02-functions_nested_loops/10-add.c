500825a64c8d23a35d2efa103cce4cdfc112b8d5
RayBar72 <3963@holbertonschool.com>
2021-10-05 18:48:40 -0700

    Q10, A0

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - prints 1 when lowercase, else 0
+ * @x: integer to be evaluated
+ * @y: integer to be evaluated
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int add(int x, int y)
+{
+
+	int z;
+
+	z = x + y;
+	return (z);
+}
