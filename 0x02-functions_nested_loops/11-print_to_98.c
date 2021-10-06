577e50c04dbac4d7a49fc5003185e0874c09308e
RayBar72 <3963@holbertonschool.com>
2021-10-05 18:28:19 -0700

    Q11, A0

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints 1 when lowercase, else 0
+ * @x: integer to be evaluated
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+
+void print_to_98(int x)
+{
+
+	int x;
+
+	for (x; x <= 97; x++)
+		printf("%i, ", x);
+	putchar('9');
+	putchar('8');
+	putchar('\n');
+	return (0);
+}
