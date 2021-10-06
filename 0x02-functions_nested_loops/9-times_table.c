434bb310f6bc6273b41cc818e237ea2d422ea174
RayBar72 <3963@holbertonschool.com>
2021-10-06 13:21:19 -0700

    Q9, A8

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,56 +2,52 @@
 
 /**
  * times_table - prints 1 when lowercase, else 0
+ * integer to be evaluated
+ *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
+
 void times_table(void)
 {
-	int x, int y, int z, int a, int b;
+	int x;
+	int y;
+	int z;
+	int a;
+	int b;
+
 
 	for (x = 0; x <= 9; x++)
-	{
-		for (y = 0; y <= 9; y++) {
+		for (y = 0; y <= 9; y++)
+		{
 			z = x * y;
 			a = z % 10;
 			b = z / 10;
-			if (z == 0) {
-				if (x == 0 && y == 9) {
+			if (y != 0)
+			{
+				if (y == 9)
+				{
 					_putchar(',');
 					_putchar(' ');
-					_putchar(' ');
-					_putchar('0');
-					_putchar('\n');	}
-				if (y == 0)
-					_putchar('0');
-				if (x == 0 && y != 9 && y != 0) {
+					if (z <= 9)
+						_putchar(' ');
+				}
+				if (y != 9 && y > 0)
+				{
 					_putchar(',');
 					_putchar(' ');
-					_putchar(' ');
-					_putchar('0'); } }
-			if (z != 0 && z <= 9) {
-				if (x == 1 && y == 9) {
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar('9');
-					_putchar('\n'); }
-				else {
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(z + '0'); } }
-			if (z > 9) {
-				if (y == 9) {
-					_putchar(',');
-					_putchar(' ');
-					_putchar(b + '0');
-					_putchar(a + '0');
-					_putchar('\n'); }
-				else {
-				_putchar(',');
-				_putchar(' ');
+					if (z <= 9)
+						_putchar(' ');
+				}
+			}
+			if (z <= 9)
+				_putchar(a + '0');
+			if (z > 9)
+			{
 				_putchar(b + '0');
-				_putchar(a + '0'); } } }
-	}
+				_putchar(a + '0');
+			}
+			if (y == 9)
+				_putchar('\n');
+		}
 }
