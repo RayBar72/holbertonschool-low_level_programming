b068f0f3f723ba84b1b50bb07f4b10b69f514e72
RayBar72 <3963@holbertonschool.com>
2021-10-07 15:40:03 -0700

    Q6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,7 +2,7 @@
 
 /**
  * print_line - writes the character c to stdout
- *
+ *@n: number of lines
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
@@ -13,9 +13,17 @@ void print_line(int n)
 
 	int z;
 
-	if (n > 0)
-		for (z = 1; z <= n; ++z)
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (z = 1; z <= n; z++)
+		{
 			_putchar('_');
+		}
+	}
 	_putchar('\n');
 
 }
