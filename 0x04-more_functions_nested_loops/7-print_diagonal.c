946d3b03e5819e0eb0ceddc7f52dd502eca3a189
RayBar72 <3963@holbertonschool.com>
2021-10-07 15:12:52 -0700

    Q7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * print_diagonal - writes the character c to stdout
+ *
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+void print_diagonal(int n)
+{
+
+	int m;
+	int o;
+
+	if (n < 0)
+		n = 0;
+
+	if (n == 0)
+		_putchar('\n');
+
+	if (n == 1)
+	{
+		_putchar('\\');
+		_putchar('\n');
+	}
+
+	if (n > 1)
+	{
+		for (o = 1; o <= n; ++o)
+		{
+			for (m = 2; m <= o; ++m)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
