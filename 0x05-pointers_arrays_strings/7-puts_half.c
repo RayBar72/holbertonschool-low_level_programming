d003c57c34eabb98e22d92dcf8c9cf74e328898c
RayBar72 <3963@holbertonschool.com>
2021-10-12 09:58:58 -0700

    Q7, A0

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts_half - Prints the second half of the string
+ * @str: string.
+ */
+
+void puts_half(char *str)
+{
+	int i, count = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+		count++;
+
+	if (count % 2 == 0)
+		for (i = (count / 2); i < count; i++)
+			_putchar(str[i]);
+	if (count % 2 != 0)
+		for (i = ((count - 1) / 2); i < count; i++)
+			_putchar(str[i]);
+	_putchar('\n');
+}
