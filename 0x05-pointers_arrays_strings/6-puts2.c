5d9d7eea277a38654389655819ef3a11c233929e
RayBar72 <3963@holbertonschool.com>
2021-10-12 15:57:30 -0700

    Q6, A0

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts2 - Function that prints every other character of a string
+ * @str: String to be printed
+ */
+
+void puts2(char *str)
+{
+	int i, count = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+		count ++;
+	i = 0;
+	do
+	{
+		_putchar(*(str + i));
+		i = i + 2;
+	}
+			while (i < count);
+	_putchar('\n');
+
+}
