ab94ba833d2074e71049a883e5d927dfd29417cf
RayBar72 <3963@holbertonschool.com>
2021-10-12 10:04:15 -0700

    Q7, A2

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -16,7 +16,7 @@ void puts_half(char *str)
 		for (i = (count / 2); i < count; i++)
 			_putchar(str[i]);
 	if (count % 2 != 0)
-		for (i = ((count - 2) / 2); i < count; i++)
+		for (i = ((count) / 2); i < count; i++)
 			_putchar(str[i]);
 	_putchar('\n');
 }
