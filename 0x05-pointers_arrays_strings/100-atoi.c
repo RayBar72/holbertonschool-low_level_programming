e8ce19a3517fb0a1e6736185261d61f38ea6883c
RayBar72 <3963@holbertonschool.com>
2021-10-13 14:34:45 -0700

    Q100, A1

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *  _atoi - fuction that translates character to int
+ * int _atoi - fuction that translates character to int
  * @s: string to be translated
  * Return: value of the int
  */
@@ -11,19 +11,16 @@ int _atoi(char *s)
 	int i, j, emp = 0, signo = 0, exp = 1, numero = 0, len = 0;
 
 	for (i = 0; s[i] < 48 || s[i] > 57; i++)
-	{
 		emp++;
-	}
 	for (j = emp; !(s[j] < 48 || s[j] > 57); j++)
-	{
-		len++;
-	}
+	   len++;
 	i = 0;
 	while (i < len)
 	{
 		j = emp + len - 1;
-		numero = (exp * ((s[j - i]) - 48)) + numero;
-		exp = exp * 10;
+		numero = (exp * ((s[j - i])-48)) + numero;
+		if (numero < 214748364.7)
+			exp = exp * 10;
 		i++;
 	}
 	i = 0;
@@ -35,5 +32,7 @@ int _atoi(char *s)
 	}
 	if (signo % 2 != 0)
 		numero = numero * (-1);
+	if (len == 0)
+		numero = 0;
 	return (numero);
 }
