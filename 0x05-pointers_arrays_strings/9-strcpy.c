052b743e6d6a5326b8e345242c986565cd5afc52
RayBar72 <3963@holbertonschool.com>
2021-10-12 18:24:20 -0700

    Q9, A0

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcpy - Copies the string pointed
+ * @dest: array of end.
+ * @src: array of origin.
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0, count =0;
+	for (i = 0; src[i] != '\0'; i++)
+		count++;
+	count--;
+	i = 0;
+
+	for (i = 0; i <= count; i++)
+	{
+		*(dest + i) = *(src + i);
+	}
+	return (0);
+}
