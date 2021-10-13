3a3e1c49448289eb797d5f40a90018132faf2673
RayBar72 <3963@holbertonschool.com>
2021-10-12 18:45:43 -0700

    Q9, A1

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,7 +8,8 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	int i = 0, count =0;
+	int i = 0, count = 0;
+
 	for (i = 0; src[i] != '\0'; i++)
 		count++;
 	count--;
@@ -18,6 +19,6 @@ char *_strcpy(char *dest, char *src)
 	{
 		*(dest + i) = *(src + i);
 	}
-	dest[i+1] = '\0';
+	dest[i] = '\0';
 	return (dest);
 }
