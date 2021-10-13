166c4e8dee512768f4915a57b2fc40f72e06fcbd
RayBar72 <3963@holbertonschool.com>
2021-10-13 15:43:05 -0700

    Q100, A4

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,28 +1,22 @@
 #include "main.h"
 
 /**
- * int _atoi - fuction that translates character to int
+ * _atoi - fuction that translates character to int
  * @s: string to be translated
  * Return: value of the int
  */
 
 int _atoi(char *s)
 {
-	int i, j, emp = 0, signo = 0, exp = 1, numero = 0, len = 0;
+	int i, j, emp = 0, signo = 0, exp = 1, numero = 0, len = 0, count = 0;
 
+	for (i = 0; s[i] != '\0'; i++)
+		count++;
+	count--;
 	for (i = 0; s[i] < 48 || s[i] > 57; i++)
 		emp++;
 	for (j = emp; !(s[j] < 48 || s[j] > 57); j++)
-	   len++;
-	i = 0;
-	while (i < len)
-	{
-		j = emp + len - 1;
-		numero = (exp * ((s[j - i]) - 48)) + numero;
-		if (numero < 214748364)
-			exp = exp * 10;
-		i++;
-	}
+		len++;
 	i = 0;
 	while (i < emp)
 	{
@@ -30,9 +24,18 @@ int _atoi(char *s)
 			signo++;
 		i++;
 	}
+	i = 0;
+	while (i < len)
+	{
+		j = emp + len - 1;
+		numero = (exp * ((s[j - i]) - 48)) + numero;
+		if (numero < 2147483647)
+			exp = exp * 10;
+		i++;
+	}
 	if (signo % 2 != 0)
 		numero = numero * (-1);
-	if (emp == 0 && len == 0)
+	if (count == 0)
 		numero = 0;
 	return (numero);
 }
