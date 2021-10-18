2e6350a2747baa064f708ae49fcd85ba1040665e
RayBar72 <3963@holbertonschool.com>
2021-10-18 13:26:07 -0700

    Q2, A0

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -13,21 +13,18 @@ char *_strchr(char *s, char c)
 {
 
 	int i = 0, len = 0, j = 0;
+	char *a;
 
 	for (i = 0; s[i] != '\0'; i++)
 		len++;
 
-	for (i = 0; i < len; i++)
+	for (i = 0; i < len && s[i] != c; i++)
 	{
-		if (s[i] == c && j == 0)
-		{
-			s = s + i;
-			j++;
-			break;
-		}
 	}
-	if (j == 0)
-		return (0);
+
+	if (s[i] == c && j == 0)
+		a = s + i;
 	else
-		return (s);
+		a = '\0';
+	return (a);
 }
