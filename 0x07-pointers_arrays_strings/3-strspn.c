f42d957e4c9afdbe26c70f5e7a919cbefa32fb6c
RayBar72 <3963@holbertonschool.com>
2021-10-18 09:36:35 -0700

    Q3, A1

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,25 +1,22 @@
 #include "main.h"
 
 /**
- * _strspn - Returns the number of bytes in the initial segment of s
- * @s: string to be evaluated
- * @accept: character to be searched
+ * _strspn - number of bytes in the initial segment of s
+ * @s: the string
+ * @accept: return bytes
  *
- * Return: n variable.
+ * Return: Nothing.
  */
 
 unsigned int _strspn(char *s, char *accept)
 {
-
 	int i = 0, j = 0, n = 0;
-
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; accept[j] != '\0'; j++)
 			if (s[i] == accept[j])
 				n++;
-		if (
 		if (n - 1 != i)
 			break;
 	}
