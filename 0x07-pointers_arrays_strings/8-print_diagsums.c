81769ccce9a84ae070b463fb53b07a8d988bc244
RayBar72 <3963@holbertonschool.com>
2021-10-18 16:14:51 -0700

    Q8, A0

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - number of bytes in the initial segment of s
+ * @a: the string
+ *
+ * Return: Nothing.
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i = 0, d1 = 0, d2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		d1 += a[i * (size +1)];
+		d2 += a[size - 1 + i * (size - 1)];
+	}
+	printf ("%d, %d\n", d1, d2);
+}
