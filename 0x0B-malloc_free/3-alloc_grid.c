55ec707236dd20b073ae5111a86fca8b1728833b
RayBar72 <email = 3963@holbertonschool.com>
2021-10-26 13:33:34 -0700

    q3, a5

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -39,7 +39,7 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	x = (int **)malloc(width * sizeof(int *));
+	x = (int **)malloc(height * sizeof(int *));
 	if (x == NULL)
 		return (NULL);
 	for (i = 0; i < height; i++)
