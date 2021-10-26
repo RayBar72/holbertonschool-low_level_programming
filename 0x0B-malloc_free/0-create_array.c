2821ff26494d03e866ca9e09991e8da25d72579a
RayBar72 <email = 3963@holbertonschool.com>
2021-10-26 11:53:38 -0700

    Q0, A0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ char *create_array(unsigned int size, char c)
 	char *s = malloc(size);
 	unsigned int i = 0;
 
-	if (s == NULL)
+	if (size <= 0 || s == NULL)
 		return (NULL);
 
 	for (i = 0; i < size; i++)
