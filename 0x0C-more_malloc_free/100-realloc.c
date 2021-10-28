44961dd36932b46abdad84e39534fbba0e21a888
RayBar72 <email = 3963@holbertonschool.com>
2021-10-28 15:30:20 -0700

    q100a1

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _realloc - function that allocates memory using malloc and free
+ * @ptr: original mem created with malloc
+ * @old_size: byts
+ * @new_size: byts
+ * Return: 98 if fails.
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *fin, *finre, *ptr1;
+	unsigned int i = 0;
+
+	ptr1 = ptr;
+
+	if (!ptr)
+		return (NULL);
+
+	if (old_size == new_size)
+		return (ptr1);
+
+	free(ptr1);
+
+	if (new_size == 0)
+		return (NULL);
+
+	fin = finre = malloc(new_size);
+	if (!finre)
+		return (NULL);
+
+	if (old_size > new_size)
+		old_size = new_size;
+
+	for (i = 0; i < old_size; i++)
+		*fin++ = *ptr1++;
+
+	return (finre);
+}
