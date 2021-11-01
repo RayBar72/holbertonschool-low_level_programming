dcf883854f3ac6710b8a793f4774a1793ecf79b8
RayBar72 <email = 3963@holbertonschool.com>
2021-11-01 10:17:25 -0700

    q2,a2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -4,20 +4,6 @@
 #include <string.h>
 
 /**
- * p_null_notnull -  function that prints a mensaje if null
- * @v: var
- * @f: format
- */
-
-void p_null_notnull(char *f, char *v)
-{
-	if (f)
-		printf(f, v);
-	else
-		printf(f, "(nil)");
-}
-
-/**
  * print_dog -  function that prints a struct
  * @d:pointer to dog
  */
@@ -25,11 +11,16 @@ void p_null_notnull(char *f, char *v)
 void print_dog(struct dog *d)
 {
 
-	char s2[] = "(nil)";
 
-	if (*d = NULL)
-		printf("%s\n", s2);
-	p_null_notnull("Name: %s\n", d->name);
-	printf("Age: %f\n", d->age);
-	p_null_notnull("Owner: %s\n", d->owner);
+	if (!d)
+		return;
+	if (d->name == NULL)
+		printf("Name: (nil)\n");
+	if (d->name != NULL)
+		printf("Name: %s\n", d->name);
+	printf("Age: %f\n", dog->age);
+	if (d->owner == NULL)
+		printf("Owner: (nil)\n");
+	if (d->owner != NULL)
+		printf("Owner: %s\n", dog->owner);
 }
