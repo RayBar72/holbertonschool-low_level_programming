6ac7cb0d424535023bb333eb2ab507121ebb286f
RayBar72 <email = 3963@holbertonschool.com>
2021-11-11 14:13:49 -0800

    rapido

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * tortuga - function executed before main
+ */
+
+void __attribute__ ((constructor)) tortuga()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
