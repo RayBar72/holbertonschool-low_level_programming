59aad4d67e2a8f3728210c06faeb2d7c92439def
RayBar72 <email = 3963@holbertonschool.com>
2021-11-22 08:21:08 -0800

    Asegurando free

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,13 +15,15 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (!filename)
 		return (0);
-	op = open(filename, O_RDONLY);
-	if (op < 0)
-		return (0);
 	s = malloc(letters);
 	if (!s)
 		return (0);
 
+	op = open(filename, O_RDONLY);
+	if (op < 0)
+	{
+		return (0);
+		free(s);
 	count = read(op, s, letters);
 	close(op);
 	if (count < 0)
