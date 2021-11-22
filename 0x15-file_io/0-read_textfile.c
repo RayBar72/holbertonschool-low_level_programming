3fa565c70a729e8195ffa67b384cbae98f69fffd
RayBar72 <email = 3963@holbertonschool.com>
2021-11-22 11:21:03 -0800

    ajuste

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,35 +11,30 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	char *s;
 	int op;
-	ssize_t count;
-
-	if (!filename)
-		return (0);
+	ssize_t count, w;
 
 	s = malloc(letters);
 	if (!s)
 		return (0);
 
+	if (!filename)
+		return (0);
 	op = open(filename, O_RDONLY);
 	if (op < 0)
-	{
-		count = 0;
-		free(s);
-		return (count);
-	}
+		return (0);
 	count = read(op, s, letters);
 	close(op);
 	if (count < 0)
 	{
 		count = 0;
 		free(s);
-		return (count);
+		return (0);
 	}
-	if (write(1, s, count) != count)
+	w = write(1, s, count);
+	if (w != count || w < 0)
 	{
 		return (0);
 		free(s);
-		return (count);
 	}
 	return (count);
 }
