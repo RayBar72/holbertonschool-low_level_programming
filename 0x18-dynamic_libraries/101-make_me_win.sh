5ca7930481a56e6ca25d428782507f185dca9b19
RayBar72 <3963@holbertonschool.com>
2022-01-10 14:34:40 -0800

    Make

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O /tmp/101-falsa.so https://github.com/RayBar72/holbertonschool-low_level_programming/blob/main/0x18-dynamic_libraries/101-falsa.so
+wget -P /tmp https://github.com/RayBar72/holbertonschool-low_level_programming/blob/main/0x18-dynamic_libraries/101-falsa.so
 export LD_PRELOAD=/tmp/101-falsa.so
