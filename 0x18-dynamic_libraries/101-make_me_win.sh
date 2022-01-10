69a9f91ef7002b0ea24a99771a52289bb9172cb2
RayBar72 <3963@holbertonschool.com>
2022-01-10 14:37:19 -0800

    Make

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/RayBar72/holbertonschool-low_level_programming/blob/main/0x18-dynamic_libraries/101-falsa.so
-export LD_PRELOAD=/tmp/101-falsa.so
+wget -P https://github.com/RayBar72/holbertonschool-low_level_programming/blob/main/0x18-dynamic_libraries/101-falsa.so
+export LD_PRELOAD=./101-falsa.so
