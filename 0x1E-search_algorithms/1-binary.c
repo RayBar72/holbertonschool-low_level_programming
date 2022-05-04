e4baabb1505940ff8627d533d4178853e175d975
RayBar72 <3963@holbertonschool.com>
2022-05-04 09:49:45 -0300

    Fixing Task 1

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -12,14 +12,14 @@ int binary_search(int *array, size_t size, int value)
 {
 	size_t i = 0, inic = 0, fin, inter;
 
-	if (!array)
+	if (array == NULL)
 		return (-1);
 
 	fin =  size - 1;
-	inter = (size - 1) / 2;
 
-	while (inic <= fin)
+	while (inic < fin)
 	{
+		inter = (fin + inic) / 2;
 		printf("Searching in array: ");
 		for (i = inic; i <= fin - 1; i++)
 			printf("%i, ", array[i]);
@@ -30,12 +30,10 @@ int binary_search(int *array, size_t size, int value)
 		else if (array[inter] > value)
 		{
 			fin = inter - 1;
-			inter = (fin + inic) / 2;
 		}
 		else if (array[inter] < value)
 		{
 			inic = inter + 1;
-			inter = (fin + inic) / 2;
 		}
 	}
 	printf("Searching in array: %i\n", array[inic]);
